20215ddbd898f250ae5154d514e3d97d72332dc5
Chris Fischer <fischer.christopher@gmail.com>
2012-04-07 20:20:15 -0400

    added method

--- ruby/script.rb
+++ ruby/script.rb
@@ -1,5 +1,16 @@
 #! /usr/bin/env ruby
 #
-# This is a hello world script
+# Hello world script
 puts "Hello world"
+
+# Arrays 
+a = [ 12, 4, 5, 11 ]
+puts a[0, 2]
+
+# Create a method
+def talk(name)
+  result = "My name is " + name 
+  return result
+end
+puts talk("Chris")
 
