2e0786e30e7780d380ccca72d7b795f1a3b1f312
Chris Fischer <fischer.christopher@gmail.com>
2012-04-10 23:04:52 -0400

    added fab file for nscd

---
+++ fabfile.py
@@ -0,0 +1,10 @@
+from fabric.operations import sudo,run
+
+def hostname():
+  sudo('hostname', pty=True)
+
+def nscd_status():
+  sudo('/etc/init.d/nscd status', pty=True)
+
+def nscd_flush():
+  sudo('/usr/sbin/nscd -i hosts', pty=True)
